b21621bad1af0439c2f442cb02b5d65cc87cb757
Catalina Portilla <gcportilla@iemsantateresita.edu.co>
2022-09-08 11:53:34 -0500

    Initial commit

---
+++ app/src/main/java/com/stasoft/checklistcem/ActivityInicioPantalla.kt
@@ -0,0 +1,11 @@
+package com.stasoft.checklistcem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityInicioPantalla : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_pantalla)
+    }
+}

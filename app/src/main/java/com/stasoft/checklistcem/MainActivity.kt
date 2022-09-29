57903d49af88389586b703c21fcee740d228954d
Punto Vive <jprodriguez@iemsantateresita.edu.co>
2022-09-29 13:58:45 -0500

    pantalla inicio

--- app/src/main/java/com/stasoft/checklistcem/MainActivity.kt
+++ app/src/main/java/com/stasoft/checklistcem/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var banding: Any
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        banding = ActivityMainBinding.inflatec
         setContentView(R.layout.activity_main)
     }
 }
+
+

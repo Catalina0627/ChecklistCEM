49fc4e996b067bf97acdcdb80b4535cbb5f018c4
Catalina Portilla <gcportilla@iemsantateresita.edu.co>
2022-11-15 11:33:21 -0500

    ultimos ajustes

--- app/src/main/java/com/stasoft/checklistcem/Activity_inicio.kt
+++ app/src/main/java/com/stasoft/checklistcem/Activity_inicio.kt
@@ -1,11 +1,18 @@
 package com.stasoft.checklistcem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class Activity_inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        sleep(3000)
+
+        val intent = Intent(this, RegistroActivity::class.java)
+        startActivity(intent)
     }
 }
